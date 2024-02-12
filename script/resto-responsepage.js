954c450a40ea7ce31a2841d4f5948f00bf866049
China <ma_china_ortiz@dlsu.edu.ph>
2024-02-13 04:17:12 +0800

    [resto response] DONE

--- script/resto-responsepage.js
+++ script/resto-responsepage.js
@@ -11,17 +11,19 @@ let photoContainer;
 function injectQuill(element) {
     const reviewPanel = element.closest('.review-panel');
     const replyPanel = reviewPanel.parentElement.nextElementSibling;
+    toggleReplyPanel(replyPanel)
 
+
+   
+}
+
+function toggleReplyPanel(replyPanel){
     
-       // Check if the element is currently hidden
     if ($(replyPanel).is(':hidden')) {
-        // If hidden, show the element
         $(replyPanel).show();
     } else {
-        // If visible, hide the element
         $(replyPanel).hide();
     }
-   
 }
 
 
@@ -30,18 +32,36 @@ function toggleReplyEditor(button) {
     replyEditor.style.display = replyEditor.style.display === 'none' ? 'block' : 'none';
 }
 
-function handleReply() {
-    const replyEditorContent = quillEditor.root.innerHTML.trim();
+function handleReply(button) {
+    var responsePanel = button.parentElement.nextElementSibling;
+    var resPanels = document.getElementsByClassName('responsepanel')
 
-    if (replyEditorContent === '') {
-        alert('Please type your reply.');
-    } 
-    
-    else {
-        alert('Reply Published!');
-        
-        quillEditor.root.innerHTML = '';
+
+    var nthNumber = 0;
+
+    for (var i = 0; i < resPanels.length; i++) {
+        if (resPanels[i] === responsePanel) {
+            break;
+        }
+        if (resPanels[i].nodeType === 1) {
+            nthNumber++;
+        }
     }
+    console.log(nthNumber)
+
+
+    var quill = quills[nthNumber]
+
+    if(quill.getText().trim().length){
+        $(responsePanel).show();     
+        var html = quills[nthNumber].root.innerHTML;    
+        responsePanel.querySelector("#replytext").innerHTML = html;
+    }
+    
+    toggleReplyPanel(responsePanel.previousElementSibling)
+
+    
+    
 }
 
 
