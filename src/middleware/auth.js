195bbfed15fdac11324284df0dd5ec8ed6291c82
Ma. China Ortiz <114428986+cookietriestoprogram@users.noreply.github.com>
2024-03-14 09:52:16 +0800

    fix navbar

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,7 @@ function isAuthenticated(req, res, next) {
     res.locals.loggedIn = req.session.userId ? true : false;
     res.locals.profilePicture = req.session.profilePicture || 'defaultProfilePic.png'; // Adjust default profile picture as necessary
     res.locals.isResto = req.session.isResto;
-    res.locals.username = req.session.username
+    res.locals.loggedUsername = req.session.username
     next();
 }
 
