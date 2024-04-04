a3a4573fc126cbeb5cfac2ee4cf25df0ea8b3eae
Ma. China Ortiz <114428986+cookietriestoprogram@users.noreply.github.com>
2024-04-05 01:49:33 +0800

    stable

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,19 +1,12 @@
 
 function isAuthenticated(req, res, next) {
-    const acceptedPaths = ['/createrestaurant', '/editrestaurant', '/resto-responsepage/:id/delete', '/resto-responsepage/:id/updatepicture', '/editrestopassword', '/logout', '/review/:_restaurantId/:_reviewId/reply/create', '/review/:_restaurantId/:_reviewId/reply/:_replyId/edit', '/review/:_restaurantId/:_reviewId/reply/:_replyId/delete'];
-    // console.log(req.session)
     res.locals.loggedIn = req.session.userId ? true : false;
     res.locals.profilePicture = req.session.profilePicture || 'defaultProfilePic.png'; // Adjust default profile picture as necessary
     res.locals.isResto = req.session.isResto;
     res.locals.loggedUsername = req.session.username
     res.locals.userId = req.session.userId;
 
-    if (res.locals.isResto && req.path.indexOf("/resto-responsepage") === -1) {
-        res.redirect('/resto-responsepage/' + req.session.userId);
-    }
-    else {
         next();
-    }
 }
 
 module.exports = {isAuthenticated};
